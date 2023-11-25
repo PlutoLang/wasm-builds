5135a4ec76ddb3e7faf369d04a93a19711287e20
Sainan <sainan@calamity.gg>
2023-11-25 13:31:47 +0100

    Add manifest.json

--- index.php
+++ index.php
@@ -40,3 +40,24 @@ copy("repos/".$repo."/".$repo.".wasm", "out/".$repo."/".$tag."/".$repo.".wasm");
 
 // Clean up
 passthru("rm -r repos/".$repo."/bin");
+
+// Update manifest
+$json = [];
+foreach ($repos as $repo => $_)
+{
+	$json[$repo] = [];
+	foreach (scandir("out/".$repo) as $tag)
+	{
+		if (substr($tag, 0, 1) != ".")
+		{
+			$version = $tag;
+			if (substr($version, 0, 1) == "v")
+			{
+				$version = substr($version, 1);
+			}
+			$json[$repo][$version] = "https://wasm.pluto.do/out/$repo/$tag/$repo.js";
+		}
+	}
+}
+file_put_contents("manifest.json", json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+
