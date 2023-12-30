7f5015ba96b62b6063dc7b823d2618db61c630ea
Sainan <sainan@calamity.gg>
2023-12-30 12:39:26 +0100

    Add libpluto

---
+++ build/libpluto.php
@@ -0,0 +1,48 @@
+<?php
+$clang = "em++ -O3 -flto -std=c++17 -fvisibility=hidden -D PLUTO_ILP_ENABLE -D PLUTO_USE_SOUP -DPLUTO_C_LINKAGE";
+
+// Setup folders
+if(!is_dir("bin"))
+{
+	mkdir("bin");
+}
+if(!is_dir("bin/int"))
+{
+	mkdir("bin/int");
+}
+
+// Find work
+$files = [];
+foreach(scandir("src/vendor/Soup/soup") as $file)
+{
+	if(substr($file, -4) == ".cpp")
+	{
+		$name = substr($file, 0, -4);
+		array_push($files, "vendor/Soup/soup/".$name);
+	}
+}
+foreach(scandir("src") as $file)
+{
+	if(substr($file, -4) == ".cpp")
+	{
+		$name = substr($file, 0, -4);
+		if($name != "lua" && $name != "luac")
+		{
+			array_push($files, $name);
+		}
+	}
+}
+
+echo "Compiling...\n";
+$objects = [];
+foreach($files as $file)
+{
+	echo $file."\n";
+	passthru("$clang -c src/$file.cpp -o bin/int/".basename($file).".o");
+	array_push($objects, escapeshellarg("bin/int/".basename($file).".o"));
+}
+
+echo "Linking...\n";
+$clang .= " -s WASM=1 -s MODULARIZE=1 -s EXPORT_NAME=libpluto -s EXPORTED_RUNTIME_METHODS=[\"FS\",\"cwrap\"] -s FS_DEBUG=1";
+//$clang .= " -s LINKABLE=1 -s EXPORT_ALL=1"; // uncomment for debugging
+passthru("$clang -o libpluto.js ".join(" ", $objects));
--- common.php
+++ common.php
@@ -1,5 +1,6 @@
 <?php
 $repos = [
 	"pluto" => "https://github.com/PlutoLang/Pluto",
+	"libpluto" => "https://github.com/PlutoLang/Pluto",
 	"lua" => "https://github.com/lua/lua",
 ];
